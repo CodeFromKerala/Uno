16aac0ce977aebd6b4bc62dad6c939d4197497ce
Madhav <mjnair52@gmail.com>
2025-08-09 15:05:01 +0530

    Documentation Added, Code Cleaned Up

--- UnoCard.java
+++ UnoCard.java
@@ -1,16 +1,30 @@
+/*
+
+UnoCard Class for cleaner and more readable code
+
+*Made In Java :)*
+
+*/ 
+
+// UnoCard Class
 public class UnoCard{
 	String color;
 	String type;
 	int value;
 	static String[] colorVal = {"Green", "Yellow", "Red", "Blue"};
-	static String[] typeVal = {"Number", "+2", "+4", "Wild", "Number", "Number"}; // number added multiple times cus i cant bring myself to think of a better solution (number card should have more probability of appearing)
+	static String[] typeVal = {"Number", "+2", "+4", "Wild", "Number", "Number"}; 
+	/* number added multiple times cus i cant bring myself to think of a better/cleaner solution 
+	(number card should have more probability of appearing)
+	*/
 
+	// Constructor Method
 	UnoCard(String color, String type, int value){
 		this.color = color;
 		this.type = type;
 		this.value = value;
 	}
 	
+	// Card Generating Method
 	static UnoCard generateCard(){
 		int colorType = (int)(Math.random() * 3);
 			int typeType = (int)(Math.random() * 3);
@@ -23,6 +37,7 @@ public class UnoCard{
 			return card;
 	}
 
+	// For Displaying Card
 	String displayVal(){
 		return this.color + "," + this.type + "," + String.valueOf(this.value);
 	}
